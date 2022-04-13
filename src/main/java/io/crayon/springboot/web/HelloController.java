876cf897d7420fbea6a8740e0b3eda97990f3661
JiHyeonBeak <jadehl09@gmail.com>
2022-04-13 23:17:08 +0900

    Test code DTO

--- src/main/java/io/crayon/springboot/web/HelloController.java
+++ src/main/java/io/crayon/springboot/web/HelloController.java
@@ -1,7 +1,9 @@
 package io.crayon.springboot.web;
 
+import io.crayon.springboot.web.dto.HelloResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller // JSON을 반환하는 컨트롤러를 선언
@@ -10,5 +12,12 @@ public class HelloController {
     @GetMapping("/hello")   // Get 요청에 대한 Mapping
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,@RequestParam("amount") int amount) {
+        /* RequestParam 외부에서 API로 넘긴 파라미터를 가져오는 어노테이션.
+         name으로 넘어온 값을 String name에 저장 */
+        return new HelloResponseDto(name,amount);
     }
 }
--- src/test/java/springboot/web/HelloControllerTest.java
+++ src/test/java/springboot/web/HelloControllerTest.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import springboot.web.dto.HelloResponseDtoTest;
+import static org.hamcrest.Matchers.is;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)    // 테스트 실행자로 SpringRunner를 사용하겠다고 선언. 스프링부트 테스트와 JUnit과의 연결자
 @WebMvcTest(controllers = HelloController.class)    // 스프링 웹 테스트에 집중할 수 있는 어노테이션. 서비스,컴포넌트,리포지토리 어노테이션은 사용불가.
@@ -24,5 +26,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))  // MockMvc를 통해/hello로 GET요청을 한다.
                 .andExpect(status().isOk()) // 헤더의 Status를 검증한다. 200인지...
                 .andExpect(content().string(hello)); // 컨트롤러의 리턴값을 검증
+    }
+
+    @Test
+    public void HelloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000 ;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name",name) // API테스트를 위한 요청 파라미터 설정. String만 허용된다.
+                        .param("amount",String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name",is(name))) // Json Response를 필드별로 검증할 수 있는 메소드. $를 기준으로 필드를 구분한다.
+                .andExpect(jsonPath("$.amount",is(amount)));
     }
 }
